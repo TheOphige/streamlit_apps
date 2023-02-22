8054402c077360e0d9156cdefb93f6525e7dca4b
TheOphige <igetheophilus02@gmail.com>
2023-02-22 22:15:56 +0100

    beautify graph

--- penguin_app/penguins.py
+++ penguin_app/penguins.py
@@ -7,8 +7,8 @@ st.title("Palmer's Penguins")
 
 st.markdown('Use this Streamlit app to make your own scatterplot about penguins!')
 
-selected_species= st.selectbox('What species would you like to visualize?',
-['Adelie', 'Gentoo', 'Chinstrap'])
+#selected_species= st.selectbox('What species would you like to visualize?',
+#['Adelie', 'Gentoo', 'Chinstrap'])
 selected_x_var= st.selectbox('What do want the x variable to be?',
 ['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm','body_mass_g'])
 selected_y_var = st.selectbox('What about the y?',
@@ -18,10 +18,17 @@ selected_y_var = st.selectbox('What about the y?',
 penguins_df = pd.read_csv('penguins.csv')
 #st.write(penguins_df.head())
 
-penguins_df= penguins_df[penguins_df['species'] == selected_species]
+#penguins_df= penguins_df[penguins_df['species'] == selected_species]
+
+sns.set_style('darkgrid')
+markers= {"Adelie": "X", "Gentoo": "s", "Chinstrap":'o'}
 
 fig, ax = plt.subplots()
-ax = sns.scatterplot(x= penguins_df[selected_x_var], y= penguins_df[selected_y_var])
+ax = sns.scatterplot(data=penguins_df, x= selected_x_var, 
+y= selected_y_var, hue='species', markers= markers, style='species')
 plt.xlabel(selected_x_var)
 plt.ylabel(selected_y_var)
+#plt.title('Scatterplot of {} Penguins'.format(selected_species))
+plt.title("Scatterplot of Palmer's Penguins")
+
 st.pyplot(fig)
