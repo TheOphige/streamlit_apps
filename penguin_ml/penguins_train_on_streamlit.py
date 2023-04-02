7bf1621be0a6757c5658c2b42f185fbc9c1f6e44
TheOphige <igetheophilus02@gmail.com>
2023-04-02 12:48:30 +0100

    reduce height

--- penguin_ml/penguins_train_on_streamlit.py
+++ penguin_ml/penguins_train_on_streamlit.py
@@ -18,7 +18,7 @@ def load_lottieurl(url: str):
     return r.json()
 
 lottie_penguin = load_lottieurl('https://assets1.lottiefiles.com/private_files/lf30_lntyk83o.json')
-st_lottie(lottie_penguin, speed= 1.5, width= 800, height= 400)
+st_lottie(lottie_penguin, speed= 1.5, width= 400, height= 200)
 
 
 # title and instructions
